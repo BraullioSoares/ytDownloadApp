0ed338d232282840d911b185bf10788648806c9d
Braullio Soares <braulliosoares.bs@gmail.com>
2019-09-15 16:41:31 -0300

    Fix port number

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
-const port = process.env.PORT | 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 
