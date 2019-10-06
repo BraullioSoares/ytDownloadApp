1a1856d698bfe8d81cff1e8016079e3dbb84e132
Braullio Soares <braulliosoares.bs@gmail.com>
2019-10-06 00:28:16 -0300

    Fix port for heroku

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
-const port = 4000;
+const serverPort = process.env.PORT || 1337;
 
 app.use(cors());
 app.use('/static', express.static(__dirname + '/public'));
-app.listen(process.env.PORT || 5000, '0.0.0.0');
+app.listen(serverPort, function () {
+    console.log("Express is running on port " + serverPort);
+});
 
 app.get('/download', (req, res) => {
     var URL = req.query.URL;
