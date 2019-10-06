2f2de3dcacfa4335376076d93e2abfd05bd7b87b
Braullio Soares <braulliosoares.bs@gmail.com>
2019-10-06 00:10:09 -0300

    Fix port for heroku

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const port = 4000;
 
 app.use(cors());
 app.use('/static', express.static(__dirname + '/public'));
-app.listen(port, () => {
-    console.log("Server is runnin on port " + port);
-});
-
+app.listen(process.env.PORT || 5000);
 
 app.get('/download', (req, res) => {
     var URL = req.query.URL;
